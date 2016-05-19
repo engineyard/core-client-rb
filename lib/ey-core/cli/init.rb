9bb6f49e141c2b53ebb116d56bbe3cdc220a5f7c
Eugene Howe <eugene@xtreme-computers.net>
2016-05-18 21:58:45 -0400

    add deprecated scp command

--- lib/ey-core/cli/init.rb
+++ lib/ey-core/cli/init.rb
@@ -1,6 +1,9 @@
 class Ey::Core::Cli::Init < Ey::Core::Cli::Subcommand
   title "init"
   summary "Deprecated"
+  description <<-DESC
+   The init command has been deprecated.  We apologize for any inconvenience.
+  DESC
 
   def handle
     abort "This command is deprecated".red

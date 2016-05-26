02d17f039d28b85fe89362d9e1238f6863f5b6f0
Dennis Walters <dwalters@engineyard.com>
2016-05-26 14:09:22 -0400

    Converted Cli to a namespace
    In order to make refactoring and such a bit more sane, this moves
    the meat of the former Cli class to Cli::Main and leaves Cli as
    a wrapper module.

--- lib/ey-core/cli.rb
+++ lib/ey-core/cli.rb
@@ -1,24 +1,8 @@
-require 'optparse'
-require 'ostruct'
-require 'ey-core'
-require 'awesome_print'
-require 'pry'
-require 'belafonte'
-require 'table_print'
-require 'rubygems/package'
-require 'escape'
+module Ey
+  module Core
 
-Cistern.formatter = Cistern::Formatter::AwesomePrint
-
-
-class Ey::Core::Cli < Belafonte::App
-  title "Engineyard CLI"
-  summary "Successor to the engineyard gem"
-
-  require_relative "cli/subcommand"
-  Dir[File.dirname(__FILE__) + '/cli/*.rb'].each {|file| load file }
-
-  Ey::Core::Cli::Subcommand.descendants.each do |d|
-    mount d
+    # The overall namespace for CLI-related code
+    module Cli
+    end
   end
 end

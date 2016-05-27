9fbe929fd0d6b240a3b854e67ff09cc6229db1df
Dennis Walters <dwalters@engineyard.com>
2016-05-27 19:22:01 -0400

    Basic spec for Cli::Recipes::Upload

---
+++ spec/ey-core/cli/recipes/upload_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+require 'ey-core/cli'
+require 'ey-core/cli/errors'
+require 'ey-core/cli/subcommand'
+require 'ey-core/cli/recipes'
+require 'ey-core/cli/recipes/upload'
+
+describe Ey::Core::Cli::Recipes::Upload do
+  set_up_cli
+
+  before(:each) do
+    allow_any_instance_of(described_class).
+      to receive(:run_chef).
+      with(any_args).
+      and_return(true)
+
+    allow_any_instance_of(described_class).
+      to receive(:upload_recipes).
+      with(any_args).
+      and_return(true)
+  end
+
+  context 'ey-core recipes upload' do
+    it 'advises that it is uploading recipes for the current environment' do
+      execute
+
+      expect(standard_output).
+        to match(/Uploading custom recipes for /)
+    end
+
+    it 'uploads the recipes' do
+      expect(cli).to receive(:upload_recipes).with(environment, 'cookbooks/')
+
+      execute
+    end
+    
+    context 'successfully' do
+      it 'advises that the upload completed' do
+        execute
+
+        expect(standard_output).
+          to match(/Uploading custom recipes complete/)
+      end
+    end
+
+    context 'unsuccessfully' do
+      before(:each) do
+        allow(cli).
+          to receive(:upload_recipes).
+          with(environment, 'cookbooks/').
+          and_raise('big bada boom')
+      end
+
+      it 'aborts, advising that the upload failed' do
+        status = execute
+
+        expect(error_output).
+          to match(/There was a problem uploading the recipes/)
+
+        expect(status).not_to eql(0)
+      end
+    end
+  end
+
+  context 'ey-core recipes upload --apply' do
+    arguments '--apply'
+
+    it 'performs a custom chef run' do
+      expect(cli).to receive(:run_chef).with('custom', environment)
+
+      execute
+    end
+  end
+
+  context 'ey-core recipes upload --environment 12345' do
+    before(:each) do
+    let(:environment2) {create_environment(account: account, client: client, environment: {id: 12345})}
+  end
+end

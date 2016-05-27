e7a51fc84c123b5c387e54975390bd22049376f9
Dennis Walters <dwalters@engineyard.com>
2016-05-27 13:54:10 -0400

    Converted raw CLI mocks to Core mocks
    In order to (hopefully) expose changes in Core that break the
    CLI app

--- spec/support/cli_helpers.rb
+++ spec/support/cli_helpers.rb
@@ -1,3 +1,6 @@
+require 'belafonte'
+require 'stringio'
+
 def set_up_cli
   let(:argv) {[]}
   let(:stdout) {StringIO.new}
@@ -6,6 +9,19 @@ def set_up_cli
   let(:kernel) {FakeKernel.new}
   let(:cli) {described_class.new(argv, stdin, stdout, stderr, kernel)}
   let(:execute) {cli.execute!}
-  let(:operator) {Object.new}
-  let(:environment) {Object.new}
+  let(:client) {create_client}
+  let(:account) {create_account(client: client)}
+  let(:app) {create_application(account: account, client: client)}
+  let(:environment) {create_environment(account: account, app: app)}
+
+  before(:each) do
+    allow_any_instance_of(described_class).
+      to receive(:core_operator_and_environment_for).
+      with(any_args).
+      and_return([client, environment])
+  end
+end
+
+def arguments(command_line)
+  let(:argv) {command_line.split(/\s+/)}
 end

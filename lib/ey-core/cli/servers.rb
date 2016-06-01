42ca69f020e9adf8d45e847a1d4461b30b493da2
Dennis Walters <dwalters@engineyard.com>
2016-06-01 10:47:07 -0400

    Made servers command match specified behavior

--- lib/ey-core/cli/servers.rb
+++ lib/ey-core/cli/servers.rb
@@ -20,19 +20,64 @@ module Ey
           argument: "environment"
 
         def handle
-          puts TablePrint::Printer.
+          abort_on_ambiguous_environment
+
+          puts servers.empty? ?
+            "No servers were found that match your criteria." :
+            TablePrint::Printer.
             new(servers, [{id: {width: 10}}, :role, :provisioned_id]).
             table_print
         end
 
         private
+        def environment_name
+          option(:environment)
+        end
+
+        def abort_on_ambiguous_environment
+          if environment_name
+            abort "The criteria you've provided matches multiple environments. Please refine further with an account." if possible_environments.length > 1
+          end
+        end
+
+        def possible_environments
+          return @possible_environments if @possible_environments
+
+          @possible_environments = [core_client.environments.get(environment_name)].compact
+
+          @possible_environments = core_client.environments.all(name: environment_name).to_a if @possible_environments.empty?
+
+          @possible_environments
+        end
+
+        def applicable_environment
+          core_client.environments.get(option(:environment)) ||
+            core_client.environments.first(name: option(:environment))
+        end
+
         def servers
-          if option(:account)
-            core_client.servers.all(account: core_account)
-          elsif environment = option(:environment)
-            (core_client.environments.get(environment) || core_client.environments.first(name: environment)).servers.all
-          else
-            core_client.servers.all
+          @servers ||= AccountFilter.filter(
+            EnvironmentFilter.filter(
+              core_client.servers.all,
+              applicable_environment
+            ),
+            core_account
+          )
+        end
+
+        module AccountFilter
+          def self.filter(servers, account)
+            return servers unless account
+
+            servers.select {|server| server.account == account}
+          end
+        end
+
+        module EnvironmentFilter
+          def self.filter(servers, environment)
+            return servers unless environment
+
+            servers.select {|server| server.environment == environment}
           end
         end
       end

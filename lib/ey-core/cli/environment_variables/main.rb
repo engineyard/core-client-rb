dec0d1943ef17c4496718a6e374c683d63f66d40
Kyle-Law <kylelaw1121@gmail.com>
2023-01-19 14:16:38 +0800

    EnvVar: Mount list.rb to main

--- lib/ey-core/cli/environment_variables/main.rb
+++ lib/ey-core/cli/environment_variables/main.rb
@@ -1,5 +1,5 @@
 require 'ey-core/cli/subcommand'
-# require 'ey-core/cli/environment_variables/list'
+require 'ey-core/cli/environment_variables/list'
 # require 'ey-core/cli/environment_variables/create'
 # require 'ey-core/cli/environment_variables/update'
 # require 'ey-core/cli/environment_variables/destroy'
@@ -12,7 +12,7 @@ module Ey
           title "environment_variables"
           summary "Environment variables specific commands"
 
-          # mount Ey::Core::Cli::EnvironmentVariables::List
+          mount Ey::Core::Cli::EnvironmentVariables::List
           # mount Ey::Core::Cli::EnvironmentVariables::Create
           # mount Ey::Core::Cli::EnvironmentVariables::Destroy
           # mount Ey::Core::Cli::EnvironmentVariables::Update

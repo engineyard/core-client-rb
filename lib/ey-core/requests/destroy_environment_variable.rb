dd2ef4a5f16b7e3496f739e409ff2c24b5e065af
Kyle-Law <kylelaw1121@gmail.com>
2023-02-11 01:13:44 +0800

    Setup destroy_environment_variables ey-core requests method

---
+++ lib/ey-core/requests/destroy_environment_variable.rb
@@ -0,0 +1,49 @@
+class Ey::Core::Client
+  class Real
+    def destroy_environment_variable(params={})
+      id  = params["id"]
+      url = params.delete("url")
+
+      request(
+        :path   => "/environment_variables/#{id}",
+        :url    => url,
+        :method => :delete,
+      )
+    end
+  end
+
+  # class Mock
+  #   def destroy_environment_variable(params={})
+  #     extract_url_params!(params)
+  #     request_id = self.uuid
+
+  #     environment_variable_id = params["id"] || params["environment_variable"]
+
+  #     environment_variable = self.find(:environment_variables, environment_variable_id)
+
+  #     request = {
+  #       "finished_at" => nil,
+  #       "id"          => request_id,
+  #       "started_at"  => Time.now,
+  #       "successful"  => "true",
+  #       "type"        => "deprovision_environment_variable",
+  #       "resource"    => [:environment_variables, environment_variable_id, lambda do |r|
+  #         environment_variable.merge!("deleted_at" => Time.now)
+
+  #         self.data[:database_servers].values.
+  #           select { |ds| ds["environment_variable"] == url_for("/environment_variables/#{environment_variable["id"]}") }.
+  #           each { |ds| ds["deleted_at"] = Time.now }
+
+  #         r.delete("resource_url")
+  #       end],
+  #     }
+
+  #     self.data[:requests][request_id] = request
+
+  #     response(
+  #       :body   => {"request" => {id: request_id}},
+  #       :status => 201,
+  #     )
+  #   end
+  # end
+end
